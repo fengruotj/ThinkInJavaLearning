1d8fe068688978bb9fc60d90dbf462be61a37c91
MasterTj <798750509@qq.com>
2018-12-07 19:51:02 +0800

    Java 对象导论.... 2018/12/7 19:50

---
+++ src/object/extend/Shape.java
@@ -0,0 +1,20 @@
+package object.extend;
+
+/**
+ * locate object
+ * Created by MasterTj on 2018/12/7.
+ * 形状类
+ */
+public class Shape {
+    public void draw(){
+        System.out.println("Shape draw");
+    }
+
+    public void erase(){
+        System.out.println("Shape erase");
+    }
+
+    public void move(){
+        System.out.println("Shape move");
+    }
+}
